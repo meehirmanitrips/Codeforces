60b1deff7d5d403658a0818b0d645289b88731fa
Meehir Mani Tripathy <meehirmanitrips@gmail.com>
2023-01-29 14:27:34 +0530

    problem solved

---
+++ Div-2.B/B_Queries_about_less_or_equal_elements.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vl vector<long long>
+#define vvl vector<vector<long long>>
+#define mod 1000000007
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    ll a[n], b[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a, a + n);
+    ll ans[m];
+    for (int i = 0; i < m; i++)
+    {
+        ans[i] = upper_bound(a, a + n, b[i]) - a;
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
